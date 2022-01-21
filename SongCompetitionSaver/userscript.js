4c28d4a572f63e51855332cc8e26028d312d70b0
Semih <smhmmc98@gmail.com>
2022-01-21 13:41:29 +0300

    v0.1.2

--- SongCompetitionSaver/userscript.js
+++ SongCompetitionSaver/userscript.js
@@ -1,19 +1,23 @@
 // ==UserScript==
 // @name         SongCompetitionSaver
 // @namespace    http://tampermonkey.net/
-// @version      0.1.1
+// @version      0.1.2
 // @description  Save song, artist and link from song competitions by clicking a button before the game
 // @author       semihM (aka rhinoooo_)
 // @source       https://github.com/semihM/TamperMonkeyScripts/blob/main/SongCompetitionSaver
 // @updateURL    https://github.com/semihM/TamperMonkeyScripts/blob/main/SongCompetitionSaver/userscript.js
 // @supportURL   https://github.com/semihM/TamperMonkeyScripts/issues
-// @include      /^(https?)?(\:)?(\/\/)?([^\/]*\.)?song\-competition\.live/game/.*
+// @include      /^(https?)?(\:)?(\/\/)?([^\/]*\.)?song\-competition\.live/.*
 // @grant        GM_addStyle
 // ==/UserScript==
 
 document.saveSongsToFile = async function(){
     let anchor = document.getElementById("save_anchor");
-    anchor.href = "data:text/plain;charset=UTF-8," + await document.getSongTexts();
+    let rows = await document.getSongTexts();
+
+    if(rows == null) return;
+
+    anchor.href = "data:text/plain;charset=UTF-8," + rows;
     console.log(anchor);
     anchor.click();
 }
@@ -25,7 +29,7 @@ document.getSongTexts = async function() {
     {
         let viewButton = document.getElementsByClassName("mb-2 mr-2 v-btn v-btn--icon v-btn--round theme--dark v-size--default")[0];
         viewButton.click();
-        await new Promise(resolve => setTimeout(resolve, 250));
+        await new Promise(resolve => setTimeout(resolve, 150));
         container = document.getElementsByClassName("v-data-table__wrapper")[0];
         rows = container.children[0].children[2].children;
         viewButton.click();
@@ -35,6 +39,11 @@ document.getSongTexts = async function() {
     let save = "";
 
     for(let t of rows) {
+        if (t.textContent == 'No Songs found.')
+        {
+            alert("No songs submitted yet!");
+            return null;
+        }
         save += t.children[1].textContent + " (" + t.children[5].children[0].children[1].href + ")\n";
     }
     return save;
@@ -42,7 +51,7 @@ document.getSongTexts = async function() {
 
 document.buttonAdder = function() {
     let title = document.getElementsByClassName("v-card__title")[0];
-    if (title == null) setTimeout(document.buttonAdder,1000);
-    else document.getElementsByClassName("v-card__title")[0].innerHTML += `<br><button style="color:purple;border: 2px solid purple;padding:3px" id="save_songs_button" onclick="document.saveSongsToFile()">Save Current Song Links</button><a style="display:none" id="save_anchor" href="data:text/plain;charset=utf-8" download="songs.txt"></a>`
+    if (title == null || title.textContent != "Game Info") setTimeout(document.buttonAdder,1500);
+    else title.innerHTML += `<br><button style="color:purple;border: 2px solid purple;padding:3px" id="save_songs_button" onclick="document.saveSongsToFile()">Save Current Song Links</button><a style="display:none" id="save_anchor" href="data:text/plain;charset=utf-8" download="songs.txt"></a>`
 }
-setTimeout(document.buttonAdder, 2000);
+setTimeout(document.buttonAdder, 1500);
